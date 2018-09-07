9357a00cc589a48e891fa605309195ec7d76bab5
KC Green <kcgreenn@gmail.com>
2018-09-07 04:42:22 -0700

    add lyrics route and functionality

--- src/context.js
+++ src/context.js
@@ -3,25 +3,40 @@ import axios from 'axios';
 
 const Context = React.createContext();
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SEARCH_TRACKS':
+      return {
+        ...state,
+        track_list: action.payload,
+        heading: 'Search Results'
+      };
+    default:
+      return state;
+  }
+};
+
 export class Provider extends Component {
   state = {
     track_list: [],
-    heading: 'Top 10 Tracks'
+    heading: 'Top 10 Tracks',
+    dispatch: action => this.setState(state => reducer(state, action))
   };
 
   componentDidMount() {
     axios
       .get(
-        `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=10&page_size=5&country=us&f_has_lyrics=1&apikey=${
+        `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${
           process.env.REACT_APP_MM_KEY
         }`
       )
       .then(res => {
-        console.log(res);
+        // console.log(res.data);
         this.setState({ track_list: res.data.message.body.track_list });
       })
       .catch(err => console.log(err));
   }
+
   render() {
     return (
       <Context.Provider value={this.state}>
